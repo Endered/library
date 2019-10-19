6b51df6cfdd0bab5ef98500d1a9b03353943c9a6
Endered <yy56ga10ve@gmail.com>
2019-10-19 13:11:34 +0900

    fix total

--- onsite/main.cpp
+++ onsite/main.cpp
@@ -3,20 +3,9 @@
 #define rep(i, n) for(int i=0;i<(n);++i)
 #define per(i, n) for(int i=(n)-1;i>=0;--i)
 #define repa(i, n) for(int i=1;i<(n);++i)
+#define lp(i, n) rep(i, n)
 #define foreach(i, n) for(auto &i:(n))
-#define pii pair<int, int>
-#define pll pair<long long, long long>
 #define all(x) (x).begin(), (x).end()
 #define bit(x) (1ll << (x))
-const ll MOD = (ll)1e9+7;
-const int INF = (ll)1e9+7;
-const ll INFLL = (ll)1e18;
+#define MOD ((ll)1e9+7)
 using namespace std;
-template<class t>
-using vvector = vector<vector<t>>;
-template<class t>
-using vvvector = vector<vector<vector<t>>>;
-template<class t>
-using priority_queuer = priority_queue<t, vector<t>, greater<t>>;
-template<class t, class u> bool chmax(t &a, u b){if(a<b){a=b;return true;}return false;}
-template<class t, class u> bool chmin(t &a, u b){if(a>b){a=b;return true;}return false;}
